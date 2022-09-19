bb160979655de123f4118038e78611ce713a353b
Steven Newman <stevennewman@Stevens-MacBook-Pro.local>
2022-09-19 10:15:26 -0600

    09.19.22 - 10:15am LATEST UPDATE

---
+++ Master September 2022/09.01.22 BUILDING DEBUGGING/GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Steven Newman on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.title = "debugging"
+        
+        _ = 4
+        
+        
+        
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+        names.removeFirst()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
